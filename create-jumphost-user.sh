a3ad289add8ddbe8bc584cfcc799be9441bfda6e
Terry Moore <tmm@mcci.com>
2018-09-22 17:47:37 -0400

    Add features to assist gateway renames

--- create-jumphost-user.sh
+++ create-jumphost-user.sh
@@ -49,10 +49,17 @@ function _help {
 Name:	$PNAME
 
 Function:
-	Create a jumphost user for a gateway
+	Create a jumphost user for a gateway, or query info.
 
 Usage:
-	$USAGE
+	$PNAME [switches] gwnuname gwgroup
+
+	gwuname is the login name of the gateway. This user
+	name must not already exist. 
+
+	gwgroup is the login group of the gateway; it's only
+	used when creating a new gateway. If the group doesn't
+	exist, a new one will be created.
 
 Switches:
 	-h		displays help (this message), and exits.
@@ -61,14 +68,18 @@ Switches:
 
 	-D		operate in debug mode.
 
+	-Q		query information.
+
 	-j {jh-fqdn}	Fully-qualified domain of jumphost, default
 			"$OPTJHFQDN_DEFAULT"
 
-	-k {gw-pubkey}	Public key of gateway
+	-k {gw-pubkey}	Public key of gateway (ignored if -Q)
 
 	-u {jh-user}	User name to use for logging into jumphost,
 			default "$OPTJHUSER_DEFAULT"
 
+	-U {uid}	User ID to use for the new group. This
+			is only used when renaming gateways.
 .
 }
 
@@ -78,19 +89,21 @@ declare -i FIRSTKEEPALIVE=40000
 declare -i JUMPPORT=22
 
 #### argument scanning:  usage ####
-USAGE="${PNAME} -[Dhv j* k*] gw-uname gw-gname"
+USAGE="${PNAME} -[DhQv j* k* U* ] gw-uname [ gw-gname ]"
 
 declare -i OPTDEBUG=0
 declare -i OPTVERBOSE=0
+declare -i OPTQUERY=0
 declare -r OPTJHUSER_DEFAULT="$USER"
 OPTJHUSER="$OPTJHUSER_DEFAULT"
 declare -r OPTJHFQDN_DEFAULT="ec2-54-221-216-139.compute-1.amazonaws.com"
 OPTJHFQDN="$OPTJHFQDN_DEFAULT"
 declare -i OPTFIRSTUSER=20000
+declare -i OPTUID=0
 
 # scan args.
 NEXTBOOL=1
-while getopts Dnhj:k:u:v c
+while getopts Dnhj:k:Qu:U:v c
 do
 	if [ $NEXTBOOL -eq -1 ]; then
 		NEXTBOOL=0
@@ -112,7 +125,9 @@ do
 
 	j)	OPTJHFQDN="$OPTARG";;
 	k)	OPTKEY="$OPTARG";;
+	Q)	OPTQUERY=$NEXTBOOL;;
 	u)	OPTJHUSER="$OPTARG";;
+	U)	OPTUID="$OPTARG";;
 	\?)	echo "$USAGE"
 		exit 1;;
 	esac
@@ -121,8 +136,10 @@ done
 #### get rid of scanned options ####
 shift	`expr $OPTIND - 1`
 
-if [ $# -ne 2 ]; then
+if [ $OPTQUERY -eq 0 -a $# -ne 2 ]; then
 	_error "Must specify gw uname and group: $USAGE"
+elif [ $OPTQUERY -ne 0 -a $# -ne 1 ]; then 
+	_error "-Q allows only gw uname"
 fi
 
 JUMPADMIN="$OPTJHUSER"
@@ -143,6 +160,28 @@ ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" -T sudo true ||
 declare -i FOUNDUSER=0
 ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" \
 	grep -q "^$MYNAME:" /etc/passwd && FOUNDUSER=1
+
+if [ $OPTQUERY -ne 0 ]; then
+	if [ $FOUNDUSER -eq 0 ]; then
+		_error "Gateway user not found: '$MYNAME'"
+	fi
+
+	MYSSHKEY=$(
+	    ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" \
+		sudo head -1 /home/$MYNAME/.ssh/authorized_keys
+	)
+	MYPWENT=$(
+	    ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" \
+		grep "^$MYNAME:" /etc/passwd
+	)
+	_verbose "PWENT: '$MYPWENT'"
+	_verbose "SSH-key: '$MYSSHKEY'"
+	typeset -i MYUID=$(echo "$MYPWENT" | cut -d: -f 3)
+	echo "Use these options when renaming this gateway:"
+	echo "-U $MYUID -k '$MYSSHKEY'"
+	exit 0
+fi
+
 declare -i FOUNDGROUP=0
 ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" \
 	grep -q "^$MYGROUP:" /etc/group && FOUNDGROUP=1
@@ -157,18 +196,26 @@ else
 fi
 
 if [ $FOUNDUSER -eq 0 ]; then
-_verbose "Creating $MYNAME"
+    if [ $OPTUID -eq 0 ]; then
+	_verbose "Creating $MYNAME"
+	USEROPTS=
+    else
+	_verbose "Creating $MYNAME with userid $OPTUID"
+	USEROPTS="--non-unique --uid $OPTUID"
+    fi
 _debug ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" \
 	sudo useradd --comment "$MYNAME" --password "*" \
 		--gid "${MYGROUP}" \
 		--no-user-group \
 		--create-home \
+		$USEROPTS \
 		--key UID_MIN="${FIRSTUID}" "$MYNAME"
 ssh -p "$JUMPPORT" "$JUMPADMIN"@"$JUMPHOST" \
 	sudo useradd --comment "$MYNAME" --password '\*' \
 		--gid "${MYGROUP}" \
 		--no-user-group \
 		--create-home \
+		$USEROPTS \
 		--key UID_MIN="${FIRSTUID}" "$MYNAME" || \
 			_error "useradd ${MYNAME} failed"
 else
